f5ed825386105d0742c4a62b7acca0e72a7df10e
최진호 <chlwlsgh777@gmail.com>
2025-06-25 16:38:59 +0900

    JS day_1 과제

--- JavaScript/day_1.js
+++ JavaScript/day_1.js
@@ -1,5 +1,58 @@
-const a = 6;
-const b = 2;
+//Q.1
 
-console.log(`${a}*${b}=${a * b}`);
-console.log(`${a}/${b}=${a / b}`);
+// let myName = "최진호";
+// const age = "29";
+
+// console.log(myName);
+// console.log(age);
+
+//Q.2
+// let age = 25;
+// age = 30;
+
+// console.log(age);
+
+//Q.6
+// const user = { name: "Bob" };
+// user.name = "Tom";
+
+// console.log(user);
+
+//Q.7
+// let name = "최진호";
+// let age = 29;
+
+// console.log(`안녕하세요, 저는 ${name}이고 ${age}입니다.`);
+
+//Q.10
+// const fruits = ["apple", "banana"];
+// fruits[0] = "Orange";
+
+// console.log(fruits);
+
+//Q.11
+// let name = "최진호";
+
+// console.log(`안녕하세요, ${name}님!`);
+
+//Q.12
+// function sum(num1, num2) {
+//   console.log(`${num1}+${num2}=${num1 + num2}`);
+// }
+
+// sum(3, 5);
+
+//Q.13
+
+function multi(a, b) {
+  console.log(`${a}*${b}=${a * b}`);
+}
+
+function div(a, b) {
+  console.log(`${a}/${b}=${a / b}`);
+}
+let a = 6;
+let b = 2;
+
+multi(a, b);
+div(a, b);
