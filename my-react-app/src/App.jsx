d30ef435b9916399a65cc9d5e36792316d8b25bf
최진호 <chlwlsgh777@gmail.com>
2025-08-01 15:07:50 +0900

    250801

---
+++ my-react-app/src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+  const x = 1;
+  const y = 2;
+  const z = x + y;
+  return (
+    <>
+      <p>hello world</p>
+      <p>{z}</p>
+      <p>{x + y}</p>
+    </>
+  );
+}
+
+export default App;
---
+++ my-react-app/src/App4.jsx
@@ -0,0 +1,15 @@
+function App() {
+  // 일반적인 주석을 사용할 수 있음
+  // 코드도 일반적인 JS처럼 작성할 수 있음
+  const x = 10;
+  const y = 20;
+  return (
+    <>
+      {/*JSX 주석은 중괄호 안에서 사용! */}
+      <p>hello world</p>
+      <p>hello world</p>
+    </>
+  );
+}
+
+export default App;
