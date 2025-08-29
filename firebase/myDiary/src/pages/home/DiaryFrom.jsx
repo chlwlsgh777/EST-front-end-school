ef1093da01993a55fb0be81fec1051327441dc76
최진호 <chlwlsgh777@gmail.com>
2025-08-29 13:52:15 +0900

    250829 중간 업로드

---
+++ firebase/myDiary/src/pages/home/DiaryFrom.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import { useState } from "react";
+import styles from "./Home.module.css";
+
+export default function DiaryForm() {
+  const [title, setTitle] = useState("");
+  const [text, setText] = useState("");
+
+  const handleData = (event) => {
+    if (event.target.id === "diary-title") {
+      setTitle(event.target.value);
+    } else if (event.target.id === "diary-content") {
+      setText(event.target.value);
+    }
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(title, text);
+  };
+
+  return (
+    <form>
+      <label className="a11y-hidden" htmlFor="diary-title">
+        일기 제목
+      </label>
+      <input
+        className="input-style"
+        id="diary-title"
+        type="text"
+        placeholder="제목"
+        required
+        value={title}
+        onChange={handleData}
+      />
+
+      <label className="a11y-hidden" htmlFor="diary-content">
+        일기 내용
+      </label>
+      <textarea
+        className={styles["diary-textarea"]}
+        id="diary-content"
+        placeholder="오늘의 비밀은 무엇인가요?"
+        required
+        value={text}
+        onChange={handleData}
+      ></textarea>
+      <button className="black-btn" type="submit">
+        작성하기
+      </button>
+    </form>
+  );
+}
